166234f3d616c76ceaacd058a1902c3518034c3a
artem <70851610+artemdov@users.noreply.github.com>
2021-04-22 16:58:00 +0300

    pagination update

--- src/components/universal/Paginator/paginator.tsx
+++ src/components/universal/Paginator/paginator.tsx
@@ -1,5 +1,6 @@
-import React from 'react'
+import React, {useState} from 'react'
 import s from '../../users/users.module.css'
+
 
 type  PaginatorFunctionType = {
     pageSize: number,
@@ -16,16 +17,30 @@ export const Paginator = (props: PaginatorFunctionType) => {
     for (let i = 1; i <= pagesCount; i++) {
         pages.push(i)
     }
+    const portionPages = 15
+    const portionCount = Math.ceil(pagesCount / portionPages)
+    const [currentNumber, setCurrentNumber] = useState(1)
+    const leftSide = (currentNumber - 1) * portionPages
+    const rightSide = currentNumber * portionPages
 
 
     return <div>
-            {pages.map(p => {
-                return <span className={props.currentPage === p ? s.selectedPage : ''} onClick={(e) => {
-                    props.onPageChanged(p)
-                }}>{p}</span>
-            })}
+        {currentNumber > 1 &&
+        <button onClick={()=>{setCurrentNumber(currentNumber - 1)}}>Prev</button>
+        }
 
-        </div>
+        {pages
+            .filter(p => p >= leftSide && p<=rightSide)
+            .map(p => {
+            return <span className={props.currentPage === p ? s.selectedPage : ''} onClick={(e) => {
+                props.onPageChanged(p)
+            }}>{p}</span>
+        })}
+            {portionCount > currentNumber &&
+        <button onClick={()=>{setCurrentNumber(currentNumber + 1)}}>Next</button>
+        }
+
+    </div>
 }
 
 
