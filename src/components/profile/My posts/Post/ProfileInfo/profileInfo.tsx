9fa234d39f277d508a8e1a12af0af285a928239f
artem <70851610+artemdov@users.noreply.github.com>
2021-04-27 23:22:57 +0300

    added contacts in profileInfo

--- src/components/profile/My posts/Post/ProfileInfo/profileInfo.tsx
+++ src/components/profile/My posts/Post/ProfileInfo/profileInfo.tsx
@@ -1,10 +1,10 @@
-import React, {ChangeEvent} from "react";
+import React from "react";
 import s from './profile.module.css';
 import Preloader from "../../../../universal/rename/Preloader";
 import {AnyAction, Dispatch} from "redux";
 import {ProfileStatusWithHooks} from "./ProfileStatusWithHooks";
 import userPhoto from "../../../../../assets/images/user.png";
-import {photoType, photoUserType} from "../../../../../redux/profileReducer";
+import {photoType} from "../../../../../redux/profileReducer";
 
 
 export type UserProfileContactsType = {
@@ -16,6 +16,10 @@ export type UserProfileContactsType = {
     youtube: string | null
     github: string | null
     mainLink: string | null
+}
+export type ContactType = {
+    contactTitle: string
+    contactValue: string | null
 }
 export type UserProfileType = {
     aboutMe?: string
@@ -52,26 +56,42 @@ const ProfileInfo = (props: ProfileInfoType) => {
 
                 <div>
                     <div>
-                        <b>Full name</b>:{props.profile.fullName ? 'yes' : 'no'}
+                        <b>Full name</b>:{props.profile.fullName}
                     </div>
                     <div>
                         <b>Looking for a job</b>:{props.profile.lookingForAJob ? 'yes' : 'no'}
                     </div>
                     {props.profile.lookingForAJob &&
                     <div>
-                        <b>Professional skills</b>.:{props.profile.lookingForAJobDescription}
+                        <b>Professional skills</b>:{props.profile.lookingForAJobDescription}
                     </div>
                     }
                     <div>
-                        <b>About Me</b>.:{props.profile.aboutMe ? 'yes' : 'no'}
+                        <b>About Me</b>:{props.profile.aboutMe}
+                    </div>
+                    <div>
+                        <b>Contacts</b>:{props.profile.contacts && Object.keys(props.profile.contacts).map(key => {
+                        const contactsKey = props.profile?.contacts ? props.profile.contacts[key as keyof UserProfileContactsType] : ''
+                        return < Contact key={key}
+                                         contactTitle={key}
+                                         contactValue={contactsKey}
+                        />
+                    })}
                     </div>
 
                     <ProfileStatusWithHooks status={props.status}
                                             updateStatus={props.updateStatus}/>
+
                 </div>
 
             </div>
         </div>
     )
+}
+
+const Contact = ({contactTitle, contactValue}: ContactType) => {
+    return <div>
+        <b>{contactTitle}</b>: {contactValue}
+    </div>
 }
 export default ProfileInfo
